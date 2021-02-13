0643987325eb95ef672c289389f92ff83a3bf661
Nathaniel Tucker <me@ntucker.me>
2021-02-13 15:56:40 -0600

    move global styles into js

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { css } from '@linaria/core';
+
 import Home from 'pages/Home';
-import 'style/main.scss';
 
 import Theme from './Theme';
+
+export const font = css`
+  :global() {
+    @font-face {
+      font-display: swap;
+      font-family: 'Graphik';
+      font-style: normal;
+      font-stretch: normal;
+      src: url('fonts/Graphik-Regular.woff') format('woff');
+    }
+
+    body {
+      font-family: 'Graphik', 'Avenir Next', 'Helvetica Nueue', Helvetica,
+        sans-serif;
+    }
+  }
+`;
 
 // Typically place global navigation and routing layer in here
 export default function App() {
